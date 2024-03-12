a03464dd8ab0ca634d0b6ef952f41419cb5b9f71
Yuri Moens <yuri.moens@gmail.com>
2024-03-12 16:38:19 +0100

    [utils] Sort lines

--- utils/src/main/java/io/reisub/devious/utils/api/Activity.java
+++ utils/src/main/java/io/reisub/devious/utils/api/Activity.java
@@ -5,11 +5,12 @@ import lombok.Value;
 @Value
 public class Activity {
 
-  public static final Activity IDLE = new Activity("Idle");
+  public static final Activity ATTACKING = new Activity("Attacking");
   public static final Activity BANKING = new Activity("Banking");
   public static final Activity DEPOSITING = new Activity("Depositing");
+  public static final Activity EATING = new Activity("Eating");
+  public static final Activity IDLE = new Activity("Idle");
   public static final Activity WITHDRAWING = new Activity("Withdrawing");
-  public static final Activity ATTACKING = new Activity("Attacking");
 
   String name;
 }
