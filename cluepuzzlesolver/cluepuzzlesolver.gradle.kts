ca00bb943c1b0343bdf6cfdfbbdd82311cb7d0e1
Yuri Moens <yuri.moens@gmail.com>
2024-03-17 19:58:40 +0100

    [cluepuzzlesolver] Add plugin

---
+++ cluepuzzlesolver/cluepuzzlesolver.gradle.kts
@@ -0,0 +1,24 @@
+version = "1.0.1"
+
+project.extra["PluginName"] = "Sluwe Clue Puzzle Solver"
+project.extra["PluginDescription"] = "Like one of those weird Rubik's cube enthusiasts"
+
+tasks {
+    jar {
+        manifest {
+            attributes(
+                mapOf(
+                    "Plugin-Version" to project.version,
+                    "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
+                    "Plugin-Provider" to project.extra["PluginProvider"],
+                    "Plugin-Description" to project.extra["PluginDescription"],
+                    "Plugin-Dependencies" to
+                            arrayOf(
+                                nameToId("Sluwe Utils")
+                            ).joinToString(),
+                    "Plugin-License" to project.extra["PluginLicense"]
+                )
+            )
+        }
+    }
+}
