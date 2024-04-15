7da14094031d92bf4001251b67bd1c48eba994ff
Yuri Moens <yuri.moens@gmail.com>
2024-04-15 19:50:34 +0200

    [utils] Add dropAll method

--- utils/src/main/java/io/reisub/devious/utils/api/SluweInventory.java
+++ utils/src/main/java/io/reisub/devious/utils/api/SluweInventory.java
@@ -1,6 +1,9 @@
 package io.reisub.devious.utils.api;
 
+import java.util.List;
+import net.runelite.api.Item;
 import net.runelite.api.ItemID;
+import net.unethicalite.api.commons.Time;
 import net.unethicalite.api.items.Equipment;
 import net.unethicalite.api.items.Inventory;
 
@@ -55,5 +58,21 @@ public class SluweInventory {
 
   public static boolean hasItemInventoryOrEquipped(String name) {
     return Inventory.contains(name) || Equipment.contains(name);
+  }
+
+  /**
+   * Drops all items while making sure not to go over the maximum amount of actions per tick (10).
+   * Sleeps on item 9 and 19 to allow for an extra action right before and right after dropping.
+   *
+   * @param items list of items that should be dropped
+   */
+  public static void dropAll(List<Item> items) {
+    for (int i = 0; i < items.size(); i++) {
+      if (i == 9 || i == 19) {
+        Time.sleepTick();
+      }
+
+      items.get(i).drop();
+    }
   }
 }
