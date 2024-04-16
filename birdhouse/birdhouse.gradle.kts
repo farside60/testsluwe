eb40ac2ecbe1365f88c18edc6da86eba304c070f
Yuri Moens <yuri.moens@gmail.com>
2024-04-16 22:52:13 +0200

    [birdhouse] Disable transports on walk to island
    Without teleports and transports being used by pathfinding, we don't
    need to use an intermediate destination anymore.

--- birdhouse/birdhouse.gradle.kts
+++ birdhouse/birdhouse.gradle.kts
@@ -1,4 +1,4 @@
-version = "1.3.12"
+version = "1.3.13"
 
 project.extra["PluginName"] = "Sluwe Bird House"
 project.extra["PluginDescription"] = "Mass bird slaughter"
--- birdhouse/src/main/java/io/reisub/devious/birdhouse/tasks/GoToIsland.java
+++ birdhouse/src/main/java/io/reisub/devious/birdhouse/tasks/GoToIsland.java
@@ -42,13 +42,11 @@ public class GoToIsland extends Task {
 
   @Override
   public void execute() {
-    // intermediate destinations so the pathfinding doesn't use the southern mushroom
-    SluweMovement.walkTo(new WorldPoint(3692, 3844, 0));
-
     SluweMovement.walkTo(
         target,
         2,
         () -> Inventory.getAll((i) -> i.hasAction("Search")).forEach((i) -> i.interact("Search")),
+        true,
         true);
 
     TileObject rowBoat = TileObjects.getNearest(ObjectID.ROWBOAT_30915);
