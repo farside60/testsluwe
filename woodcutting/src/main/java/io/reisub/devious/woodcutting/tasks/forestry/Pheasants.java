16d0d34b1fd5c6f4047699654f555a95b33f312c
Yuri Moens <yuri.moens@gmail.com>
2024-05-18 14:41:25 +0200

    [woodcutting] Optimize Forestry pheasants event
    We now check for being stunned and wait until we're no longer stunned.
    Attempt to interrupt stealing an egg when the nest is no longer safe
    after interacting with it.

--- woodcutting/src/main/java/io/reisub/devious/woodcutting/tasks/forestry/Pheasants.java
+++ woodcutting/src/main/java/io/reisub/devious/woodcutting/tasks/forestry/Pheasants.java
@@ -9,10 +9,13 @@ import net.runelite.api.NPC;
 import net.runelite.api.NpcID;
 import net.runelite.api.ObjectID;
 import net.runelite.api.TileObject;
+import net.runelite.api.coords.WorldPoint;
 import net.unethicalite.api.commons.Time;
 import net.unethicalite.api.entities.NPCs;
+import net.unethicalite.api.entities.Players;
 import net.unethicalite.api.entities.TileObjects;
 import net.unethicalite.api.items.Inventory;
+import net.unethicalite.api.movement.Movement;
 
 public class Pheasants extends Task {
   @Inject private Woodcutting plugin;
@@ -25,12 +28,21 @@ public class Pheasants extends Task {
 
   @Override
   public boolean validate() {
-    return config.forestryPheasants() && NPCs.getNearest(NpcID.FREAKY_FORESTER_12536) != null;
+    return config.forestryPheasants()
+        && NPCs.getNearest(NpcID.FREAKY_FORESTER_12536) != null;
   }
 
   @Override
   public void execute() {
     plugin.setActivity(Woodcutting.FORESTRY);
+
+    // if we're stunned, wait until we're not anymore
+    // we check this here instead of in validate so that we don't try doing any other tasks when
+    // stunned
+    if (Players.getLocal().getModelHeight() == 1000) {
+      Time.sleepTicksUntil(() -> Players.getLocal().getModelHeight() != 1000, 10);
+      Time.sleepTick();
+    }
 
     TileObject nest = TileObjects.getNearest(ObjectID.PHEASANT_NEST_49937);
     if (nest == null) {
@@ -42,8 +54,21 @@ public class Pheasants extends Task {
       Time.sleepTick();
     }
 
+    final WorldPoint nestLocation = nest.getWorldLocation();
+
     nest.interact(0);
-    Time.sleepTicksUntil(() -> Inventory.contains(ItemID.PHEASANT_EGG), 10);
+    Time.sleepTicksUntil(
+        () ->
+            Inventory.contains(ItemID.PHEASANT_EGG)
+                || TileObjects.getFirstAt(nestLocation, ObjectID.PHEASANT_NEST_49937) == null,
+        10);
+
+    // interrupt if pheasant nest is not safe anymore
+    if (!Inventory.contains(ItemID.PHEASANT_EGG)
+        && TileObjects.getFirstAt(nestLocation, ObjectID.PHEASANT_NEST_49937) == null) {
+      Movement.walk(Players.getLocal().getWorldLocation());
+      return;
+    }
 
     NPC forester = NPCs.getNearest(NpcID.FREAKY_FORESTER_12536);
     if (forester == null) {
