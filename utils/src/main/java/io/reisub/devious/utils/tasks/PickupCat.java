1c6b454d71e295af22c8ab9fc80efe7d85d0f0e4
Yuri Moens <yuri.moens@gmail.com>
2024-05-21 16:39:46 +0200

    [utils] Fix cat pick-up and pick up kitten when out of food

--- utils/src/main/java/io/reisub/devious/utils/tasks/PickupCat.java
+++ utils/src/main/java/io/reisub/devious/utils/tasks/PickupCat.java
@@ -1,13 +1,19 @@
 package io.reisub.devious.utils.tasks;
 
+import io.reisub.devious.utils.Config;
+import javax.inject.Inject;
 import net.runelite.api.NPC;
 import net.unethicalite.api.commons.Time;
 import net.unethicalite.api.entities.NPCs;
 import net.unethicalite.api.entities.Players;
 import net.unethicalite.api.items.Inventory;
+import net.unethicalite.api.movement.Reachable;
+import net.unethicalite.api.utils.MessageUtils;
 
 public class PickupCat extends Task {
+  @Inject private Config config;
   private NPC cat;
+  private NPC kitten;
 
   @Override
   public String getStatus() {
@@ -16,6 +22,18 @@ public class PickupCat extends Task {
 
   @Override
   public boolean validate() {
+    kitten =
+        NPCs.getNearest(
+            n -> n.getName().equals("Kitten") && n.getInteracting() == Players.getLocal());
+
+    if (kitten != null
+        && KittenTask.handleKitten
+        && !Inventory.isFull()
+        && !Inventory.contains(config.kittenFood())
+        && Reachable.isInteractable(kitten)) {
+      return true;
+    }
+
     cat =
         NPCs.getNearest(
             n ->
@@ -24,13 +42,28 @@ public class PickupCat extends Task {
                     && n.getInteracting() == Players.getLocal()
                     && n.hasAction("Pick-up"));
 
-    return cat != null && !Inventory.isFull();
+    return cat != null && !Inventory.isFull() && Reachable.isInteractable(cat);
   }
 
   @Override
   public void execute() {
-    cat.interact("Pick-up");
+    if (kitten != null) {
+      pickUpKitten();
+    } else {
+      pickUpCat();
+    }
+  }
 
-    Time.sleepTicksUntil(() -> Inventory.contains("Cat"), 20);
+  private void pickUpKitten() {
+    MessageUtils.addMessage("No kitten food found, picking up kitten.");
+    kitten.interact("Pick-up");
+    Time.sleepTicksUntil(() -> Inventory.contains("Pet kitten"), 20);
+
+    KittenTask.handleKitten = false;
+  }
+
+  private void pickUpCat() {
+    cat.interact("Pick-up");
+    Time.sleepTicksUntil(() -> Inventory.contains("Pet cat"), 20);
   }
 }
